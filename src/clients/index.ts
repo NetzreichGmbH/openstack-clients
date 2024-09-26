98959a7cdb4f21b34e854ef2824dee5a0eaef417
Philipp Bucher <bucher@netzreich.de>
2024-09-26 09:59:08 +0200

    Initial Release

---
+++ src/clients/index.ts
@@ -0,0 +1,121 @@
+import * as Compute from "./compute";
+import * as Network from "./network";
+import * as Identity from "./identity";
+import { BaseAPI } from "./identity/base";
+
+
+
+export default class OpenStack {
+  Compute;
+  Identity;
+  Network;
+
+  configuration: Identity.Configuration;
+
+  _token?: string;
+
+  constructor(configuration: Identity.Configuration) {
+    this.Identity = {
+      ...Identity.AuthApiFactory(configuration),
+      ...Identity.RolesApiFactory(configuration),
+      ...Identity.UsersApiFactory(configuration),
+      ...Identity.ProjectsApiFactory(configuration),
+      ...Identity.DomainsApiFactory(configuration),
+      ...Identity.ServicesApiFactory(configuration),
+      ...Identity.EndpointsApiFactory(configuration),
+      ...Identity.GroupsApiFactory(configuration),
+    };
+    this.configuration = configuration;
+    this.Compute = {
+      ...Compute.ExtensionsApiFactory(configuration),
+      ...Compute.OsSimpleTenantUsageApiFactory(configuration),
+      ...Compute.ServersApiFactory(configuration),
+      ...Compute.FlavorsApiFactory(configuration),
+      ...Compute.ImagesApiFactory(configuration),
+      ...Compute.OsKeypairsApiFactory(configuration),
+    };
+    this.Network = {
+      ...Network.NetworksApiFactory(configuration),
+      ...Network.SubnetsApiFactory(configuration),
+      ...Network.PortsApiFactory(configuration),
+      ...Network.SecurityGroupsApiFactory(configuration),
+      ...Network.SecurityGroupRulesApiFactory(configuration),
+      ...Network.FloatingipPoolsApiFactory(configuration),
+    };
+
+  }
+
+
+  authenticate(credentials: Identity.AuthTokensPostRequest): Promise<void> {
+    return this.Identity.authTokensPost(credentials).then((res) => {
+      const token = res.headers['x-subject-token'];
+      this._token = token;
+      const catalog = res.data.token?.catalog;
+      if (!catalog) {
+        throw new Error("No catalog found in response");
+      }
+      this.initializeApi("compute", catalog);
+      this.initializeApi("network", catalog);
+      this.initializeApi("identity", catalog);
+
+    });
+  }
+
+  initializeApi(type: string,  catalog: Identity.AuthCatalogGetResponseCatalogInner[], entrypointInterface: string = "public") {
+
+    const service = catalog.find((entry) => entry.type === type);
+    if (!service || !service.endpoints) {
+      throw new Error(`No  ${type} service found in catalog`);
+    }
+    const endpoint = service.endpoints.find((endpoint) => endpoint.interface === entrypointInterface);
+    if (!endpoint || !endpoint.url) {
+      throw new Error(`No ${entrypointInterface} endpoint found in compute service`);
+    }
+    const parsedUrl = new URL(endpoint.url);
+    const config = new Identity.Configuration({
+      ...this.configuration,
+    });
+
+    config.basePath = `${parsedUrl.protocol}//${parsedUrl.host}`;
+    config.baseOptions.headers["X-Auth-Token"] = this._token;
+
+
+    if (type === "compute") {
+      this.Compute = {
+        ...Compute.ExtensionsApiFactory(config),
+        ...Compute.OsSimpleTenantUsageApiFactory(config),
+        ...Compute.ServersApiFactory(config),
+        ...Compute.FlavorsApiFactory(config),
+        ...Compute.ImagesApiFactory(config),
+        ...Compute.OsKeypairsApiFactory(config),
+      };
+    }
+
+    if (type === "network") {
+      this.Network = {
+        ...Network.NetworksApiFactory(config),
+        ...Network.SubnetsApiFactory(config),
+        ...Network.PortsApiFactory(config),
+        ...Network.SecurityGroupsApiFactory(config),
+        ...Network.SecurityGroupRulesApiFactory(config),
+        ...Network.FloatingipPoolsApiFactory(config),
+      };
+    }
+    if (type === "identity") {
+      this.Identity = {
+        ...Identity.AuthApiFactory(config),
+        ...Identity.RolesApiFactory(config),
+        ...Identity.UsersApiFactory(config),
+        ...Identity.ProjectsApiFactory(config),
+        ...Identity.DomainsApiFactory(config),
+        ...Identity.ServicesApiFactory(config),
+        ...Identity.EndpointsApiFactory(config),
+        ...Identity.GroupsApiFactory(config),
+      };
+    }
+  }
+
+  
+}
+
+export { Compute, Network, Identity };
